a96a0f130c73302e6b6798c84c17b2f4bdc5e0c3
Mdafsarx <mdafsar99009@gmail.com>
2024-04-29 16:06:35 +0600

    all craft design commplect

--- src/Pages/allArt&Craft/AllArtCraft.jsx
+++ src/Pages/allArt&Craft/AllArtCraft.jsx
@@ -1,7 +1,81 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FcRating } from "react-icons/fc";
+
 const AllArtCraft = () => {
+
+
+    const [allData, setData] = useState([])
+    useEffect(() => {
+        axios.get('https://ph-10-as-server.vercel.app/ArtCraft')
+            .then(data => {
+                setData(data.data)
+            })
+    }, []);
+
+    const viewDetails=(id)=>{
+        
+    
+    }
+
     return (
-        <div>
-            AllArtCraft
+        <div className="max-w-7xl mx-auto md:py-20">
+            <div>
+                <table className="table border-2 border-black">
+                    {/* head */}
+                    <thead className="bg-black text-white">
+                        <tr >
+                            <th className="hidden md:flex"></th>
+                            <th>User name</th>
+                            <th className="hidden md:flex">Item name</th>
+                            <th>Price</th>
+                            <th>Rating</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allData?.map((Data, i) =>
+                                <tr key={i}>
+                                    <td className="hidden md:flex">
+                                        <div className="flex items-center gap-7">
+                                            <div className="w-full">
+                                                <img src={Data?.Image} className="w-full h-60  rounded-2xl" />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p className="text-xl font-bold">
+                                           {Data?.name}
+                                        </p>
+                                    </td>
+                                    <td className=" items-center">
+                                        <p className="text-xl font-bold">
+                                           {Data?.Item}
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p className="text-xl font-bold">
+                                           {Data?.price}$
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <p className="text-xl font-bold flex items-center">
+                                           {Data?.rating}<FcRating />
+                                        </p>
+                                    </td>
+                                    <td>
+                                       <button className="btn bg-[#40F8FF]" onClick={()=>viewDetails(Data?._id)}>Details</button>
+                                    </td>
+                                </tr>)
+                        }
+
+
+
+                    </tbody>     
+
+                </table>
+            </div>
         </div>
     );
 };
